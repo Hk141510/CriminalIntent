9d008505c453c6212e784959d9bd0bc5b2a9ff04
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2017-10-04 10:20:22 -0400

    DB stuff

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -91,6 +91,10 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return mCrimes.size();
         }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
     }
 
     @Override
@@ -174,6 +178,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapater(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
 
